be630676217784c1bca4cf2fc4d08fe673b8f577
Trent Brew <tbrew1023@gmail.com>
2024-10-01 23:51:06 -0700

    collage for dr erin

---
+++ pages/collage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div style="display: flex; flex-wrap: wrap; justify-content: center; align-items: center; height: 100vh; width: 100vw; overflow: hidden; border: solid white 1.3vw; gap: 1.3vw;">
+    <div style="width: 100%; max-width: 100vw; box-sizing: border-box;">
+      <img src="https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/9ipk2xbj9zmqc7t/black_minimalist_moodboard_photo_colllage_A8aRcCYxk3.png?token=" style="width: 100%; height: auto;">
+    </div>
+    <div style="width: 100%; max-width: 100vw; box-sizing: border-box;">
+      <img src="https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/9ipk2xbj9zmqc7t/black_minimalist_moodboard_photo_colllage_A8aRcCYxk3.png?token=" style="width: 100%; height: auto;">
+    </div>
+    <div style="width: 100%; max-width: 100vw; box-sizing: border-box;">
+      <img src="https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/9ipk2xbj9zmqc7t/black_minimalist_moodboard_photo_colllage_A8aRcCYxk3.png?token=" style="width: 100%; height: auto;">
+    </div>
+  </div>
+</template>
